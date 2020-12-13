de87535a30fb1f15147fa880795e2208447aa4f0
Murt-Y <63716983+Murt-Y@users.noreply.github.com>
2020-12-14 01:35:47 +0300

    Add files via upload

---
+++ Day12/Day12.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2020
+{
+    class D12
+    {
+
+        public int ReturnResult()
+        {
+            int xcor=0;
+            int ycor=0;
+            int wxcor=10;
+            int wycor=1;
+
+            string[] Initialize()
+            {
+                
+            string[] text = File.ReadAllLines(@"C:\Users\yilma\Documents\AOC2020\Day12/input12.txt");
+            return text;
+            }
+
+
+            string[] input = Initialize();
+
+
+            void TurnL()
+        {   
+                int m=wxcor;
+                int n=wycor;
+                wycor=m;
+                wxcor=n*-1;
+        }
+            void TurnR()
+        {   
+                int m=wxcor;
+                int n=wycor;
+                wycor=m*-1;
+                wxcor=n;
+        }
+            void MoveW(char d, int x)
+            {
+                if(d=='N')
+                {
+                    wycor=wycor+x;
+                }
+                if(d=='S')
+                {
+                    wycor=wycor-x;
+                }
+                if(d=='E')
+                {
+                    wxcor=wxcor+x;
+                }
+                if(d=='W')
+                {
+                    wxcor=wxcor-x;
+                }
+            }
+            void Move(int x)
+            {
+                xcor=xcor+wxcor*x;
+                ycor=ycor+wycor*x;
+            }
+            
+            void Action(char co, int no)
+            {
+                if (co=='L')
+                {
+
+                    while(no>0)
+                    {
+                        TurnL();
+                        no=no-90;
+                    }
+                    return;
+                }
+                else if (co=='R')
+                {
+
+                    while(no>0)
+                    {
+                        TurnR();
+                        no=no-90;
+                    }
+                    return;
+                }
+                else if (co=='F')
+                {
+                    Move(no);
+                }
+                else if (co=='B')
+                {
+                    Move((-1*no));
+                }
+                else if (co=='N')
+                {
+                    MoveW('N', no);
+                }
+                else if (co=='S')
+                {
+                    MoveW('S', no);
+                }
+                else if (co=='E')
+                {
+                    MoveW('E', no);
+                }
+                else if (co=='W')
+                {
+                    MoveW('W', no);
+                }
+            
+            
+            
+            }
+            
+            foreach (string w in input)
+            {
+                char code=w[0];
+                int mag=Convert.ToInt32(w.Substring(1));
+                Action(code, mag);
+            }
+        Console.WriteLine("The Manhattan Code is ... " + (Math.Abs(xcor)+Math.Abs(ycor)));
+        return 0;
+        }
+        
+    }
+}
---
+++ Day13/Day13.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2020
+{
+    class D13
+    {
+public class Bus
+{ 
+    public long Id { get; set; }
+    public long Time { get; set; }
+}
+        public int ReturnResult()
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            string[] Initialize()
+            {
+                
+            string[] text = File.ReadAllLines(@"C:\Users\yilma\Documents\AOC2020\Day13/input13.txt");
+            return text;
+            }
+
+            string[] input = Initialize();
+            string[] bustime =input[1].Split(',');
+            
+            List<Bus> bust= new List<Bus>();
+            int i=0;
+
+            foreach(string w in bustime)
+            {
+                if(w=="x")
+                {
+                    i++;
+                }
+                else
+                {
+                    bust.Add(new Bus(){Id=Convert.ToInt64(w), Time=i});
+                    i++;
+            }
+            }
+                long CheckV(long x, long y, long z, long t)
+                {
+                    bool check=false;
+                    int j=1;
+                    while(check==false)
+                    {
+                        if(((x+y*j)+t)%z==0)
+                        {
+                            return (x+y*j);
+                        }
+                        j++;
+                    }
+                    return 0;
+                }
+                int p=2;
+                long val=CheckV(bust[0].Time, bust[0].Id, bust[1].Id, bust[1].Time);
+                long repeat=bust[0].Id*bust[1].Id;
+                while(p<bust.Count)
+                {
+                    val=CheckV(val, repeat, bust[p].Id, bust[p].Time);
+                    Console.WriteLine("The answer is " + val);
+                    repeat=repeat*bust[p].Id;
+                    p++;
+                }
+
+                watch.Stop();
+                Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        return 0;
+        }
+        
+    }
+}
