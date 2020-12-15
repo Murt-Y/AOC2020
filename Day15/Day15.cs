2bb00a21e7bf5abc390361c3684e768941324720
Murt-Y <63716983+Murt-Y@users.noreply.github.com>
2020-12-15 17:11:48 +0300

    Add files via upload

---
+++ Day15/Day15.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections; 
+
+namespace aoc2020
+{
+    class D15
+    {
+
+        public int ReturnResult()
+        {
+            string[] Initialize()
+            {
+                
+            string text = File.ReadAllText(@"C:\Users\yilma\Documents\AOC2020\Day15/input15.txt");
+            string[] words = text.Split(",");
+            return words;
+            }
+
+            string[] input = Initialize();
+            SortedList<int,int> Memory = new SortedList<int,int>();
+            int i=0;
+
+            foreach(string w in input)
+            {
+                    Memory.Add(Convert.ToInt32(w), i);
+                    i++;
+            }
+
+            int next=0;
+            while (i<30000000)
+            {
+                
+                if(i==30000000-1)
+                {
+                    Console.WriteLine(next);
+                }
+                if(Memory.ContainsKey(next)==false)
+                {
+                    Memory.Add(next, i);
+                    next=0;
+                }
+                else{
+                    int temp=next;
+                    next=i-Memory[next];
+                    Memory[temp]=i;  
+                }
+                i++;
+            }
+        return 0;
+        }
+        
+    }
+}
