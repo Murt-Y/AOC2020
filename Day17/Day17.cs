7c7d6c05209f7518a131936377120ad5509ecca9
Murt-Y <63716983+Murt-Y@users.noreply.github.com>
2020-12-25 17:13:10 +0300

    Add files via upload

---
+++ Day17/Day17.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2020
+{
+    class D17
+    {
+public class Plan
+{ 
+    public int Xcor { get; set; }
+    public int Ycor { get; set; }
+    public int Zcor { get; set; }
+    public int Wcor { get; set; }
+    public char State { get; set; }
+    public char NextState { get; set; }
+}
+        public int ReturnResult()
+        {
+            string[] Initialize()
+            {
+                
+            string[] text = File.ReadAllLines(@"/home/murat/Documents/aoc2020/Day17/input17.txt");
+            return text;
+            }
+
+            string[] input = Initialize();
+
+
+            List<Plan> coordinates = new List<Plan>();
+
+            // UNTIL HERE; COORDINATES ADD; CHECK EACH NEIGHBOUR; IF NO ADD COORD, FOR THE EDGES AT LEAST? OR INCREASE GRID NECESSARY TIMEs;
+            int row=8;
+            int row_s=0;
+            int col=8;
+            int col_s=0;
+            int dep=0;
+            int dep_s=0;
+            int wor=0;
+            int wor_s=0;
+            int y=0;
+            int x=0;
+            foreach(string w in input)
+            {   
+
+                foreach(char a in w)
+                {
+                coordinates.Add(new Plan(){Xcor=x, Ycor=y, Zcor=0, Wcor=0, State=a});
+                x++;
+                }
+                x=0;
+                y++;
+            }
+            
+            void Expand()
+            {
+                row++;
+                row_s--;
+                col++;
+                col_s--;
+                dep++;
+                dep_s--;
+                wor++;
+                wor_s--;
+
+                int j=row_s;
+                int k=col_s;
+                int l=dep_s;
+                int m=wor_s;
+
+                while(j<row)
+                {
+                    while(k<col)
+                    {
+                        while(l<=dep)
+                        {
+                            while(m<=wor)
+                            {
+                            int x=coordinates.FindIndex(x=> x.Xcor==k && x.Ycor==j && x.Zcor==l && x.Wcor==m);
+                            if(x==-1)
+                            {
+                                coordinates.Add(new Plan(){Xcor=k, Ycor=j, Zcor=l, Wcor=m, State='.'});
+                            }
+                                
+                                m++;
+                            }
+                            m=wor_s;
+                            l++;
+                        }
+                        l=dep_s;
+                        k++;
+                    }
+                    k=col_s;
+                    j++;
+                }
+                
+            }
+            void CheckN()
+            {
+                foreach (Plan c in coordinates)
+                {
+                    int activen=0;
+                    foreach (Plan n in coordinates)
+                    {
+                            if(n.State=='.')
+                            {
+                                continue;
+                            }
+                        int difx=c.Xcor-n.Xcor;
+                        if(Math.Abs(difx)>1)
+                        {
+                            continue;
+                        }
+                        int dify=c.Ycor-n.Ycor;
+                        if(Math.Abs(dify)>1)
+                        {
+                            continue;
+                        }
+                        int difz=c.Zcor-n.Zcor;
+                        if(Math.Abs(difz)>1)
+                        {
+                            continue;
+                        }
+                        int difw=c.Wcor-n.Wcor;
+                        if(Math.Abs(difw)>1)
+                        {
+                            continue;
+                        }
+
+                        if(difx== 0 && dify== 0 && difz==0 && difw==0)
+                        {
+                            continue;
+                        }
+                        activen++;
+
+                    }
+                    if (c.State=='.' && activen==3)
+                    {
+                        c.NextState='#';
+                    }
+                    else if(c.State=='#' && (activen==2 || activen==3))
+                    {
+                        c.NextState='#';
+                    }
+                    else
+                    {
+                        c.NextState='.';
+                    }
+
+                }
+            }
+
+            void Update()
+            {
+                foreach(Plan u in coordinates)
+                {
+                        u.State=u.NextState;
+                }
+            }
+
+            void Cycle()
+            {
+            Expand();
+            CheckN();
+            Update();
+            }
+            Cycle();
+            Cycle();
+            Cycle();
+            Cycle();
+            Cycle();
+            Cycle();
+
+            int activecubes=0;
+            foreach(Plan a in coordinates)
+            {
+                if(a.State=='#')
+                {
+                    activecubes++;
+                }
+            }
+        return 0;
+        }
+        
+    }
+}
---
+++ Day18/Day18.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2020
+{
+    class D18
+    {
+
+        public int ReturnResult()
+        {
+            string[] Initialize()
+            {
+                
+            string[] text = File.ReadAllLines(@"/home/murat/Documents/aoc2020/Day18/input18.txt");
+            return text;
+            }
+
+            string[] input = Initialize();
+
+    long Operation(string t)
+
+    {
+        t=t.Replace(" " , "");
+        int i=0;
+        long result=0;
+        string op="plus";
+
+           while(i<t.Length)
+            {
+                if(t[i]=='+')
+                {
+                    op="plus";
+                }
+                else if(t[i]=='*')
+                {
+                        result=result*Operation(t.Substring(i+1, t.Length-i-1));
+                        return result;
+                }
+                else if(Char.IsDigit(t[i]))
+                {
+                    if(op=="plus")
+                    {
+                        result=result+(t[i]-48);
+                    }
+
+                }
+                else if(t[i]=='(')
+                {
+
+                    long rest=Operation(t.Substring(i+1, t.Length-i-1));
+                    if(op=="plus")
+                    {
+                        result=result+rest;
+                    }
+                    if(op=="multi")
+                    {
+                        result=result*rest;
+                    }
+                    
+                    bool inp=true;
+                    int tcount=0;
+                    while (inp==true)
+                    {
+                        
+
+                        if(t[i]==')')
+                        {
+                            tcount--;
+                        }
+                        if(t[i]=='(')
+                        {
+                            tcount++;
+                        }
+                        if(tcount==0)
+                        {
+                            inp=false;
+                            i--;
+                        }
+                        i++;
+                    }
+                }
+                else if(t[i]==')')
+                {
+                    return result;
+                }
+
+                i++;
+            }
+    return result;
+    }
+long totres=0;
+foreach (string w in input)
+{
+    totres=totres+Operation(w);
+    
+}
+
+
+Console.WriteLine("Final Result is " +totres);
+
+        return 0;
+        }
+        
+    }
+}
